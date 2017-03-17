fccfd7e43106a61d81fd41221eb785bcc6031562
Adrian Dalca <adalca@mit.edu>
2017-03-17 19:48:47 -0400

    added prior to Dice loss, fixed small bugs

--- src/pytools/metrics.py
+++ src/pytools/metrics.py
@@ -1,5 +1,5 @@
 '''
-custom metrics for the fs cnn project 
+custom metrics for the neuron project 
 This module is currently very experimental...
 '''
 
@@ -7,27 +7,58 @@ import numpy as np
 import keras.backend as K
 
 # medipy package 
+import sys
+sys.path.append("C:\\Users\\adalca\\Dropbox (Personal)\\code\\python\\medipy-lib")
 import medipy.metrics
 
 class Dice(object):
     ''' UNTESTED
-    dice-based metric(s)
+    Dice-based metric(s)
 
     Variables:
         labels: optional numpy array of shape (L,) where L is the number of labels to be evaluated.
-            if not provided, all non-
+            if not provided, all non-background (0) labels are computed and averaged
+        weights: optional numpy array of shape (L,) giving relative weights of each label
+        prior: filename of spatial priors to be added to y_pred before Dice
+            TODO: maybe move this to a 'Prior' layer with a set weight in architecture ?
+
 
     Usage
         diceloss = metrics.dice([1, 2, 3])
         model.compile(diceloss, ...)
     '''
 
-    def __init__(self, labels=None):
+    def __init__(self, labels=None, weights=None, prior=None):
         self.labels = labels
+        self.weights = weights
+
+        # process prior
+        if prior is not None:
+            data = np.load(prior)
+            loc_vol = data['prior']
+            loc_vol = np.expand_dims(loc_vol, axis=0) # reshape for model
+            loc_vol /= np.sum(loc_vol, axis=-1, keepdims=True)
+            self.log_prior = np.log(loc_vol)
+        else:
+            self.log_prior = None
 
     def loss(self, y_true, y_pred):
-        ''' the loss '''
-        dicem = medipy.metrics.dice(y_true.eval(), y_pred.eval(), self.labels)
+        ''' the loss. Assumes y_pred is prob (in [0,1] and sum_row = 1) '''
+
+        y_pred_np = np.log(y_pred.eval())
+        if self.log_prior is not None:
+            y_pred_np = y_pred_np + self.log_prior
+        lab_pred = np.argmax(y_pred_np)
+        lab_true = np.argmax(y_true.eval())
+
+        # compute dice measure
+        dicem = medipy.metrics.dice(lab_true, lab_pred, self.labels)
+
+        # weight the labels
+        if self.weights is not None:
+            dicem *= self.weights
+
+        # return negative mean dice as loss
         return K.variable(-np.mean(dicem))
 
 
@@ -51,7 +82,7 @@ class CategoricalCrossentropy(object):
     '''
 
     def __init__(self, weights=None, prior=None):
-        self.weights = None if weights is None else K.variable(weights)
+        self.weights = weights if (weights is not None) else K.variable(weights)
 
         # process prior
         if prior is not None:
@@ -80,7 +111,7 @@ class CategoricalCrossentropy(object):
         if self.weights is not None:
             loss *= self.weights
 
-        loss =-K.sum(loss, -1)
+        loss = -K.sum(loss, -1)
         return K.mean(loss)
 
 
