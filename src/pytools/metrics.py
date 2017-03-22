3a43c7f8fc0ef0b24af70a189e748ad5544072f4
adalca <adalca@mit.edu>
2017-03-22 12:12:48 -0400

    clean up of metrics

--- src/pytools/metrics.py
+++ src/pytools/metrics.py
@@ -1,18 +1,80 @@
 '''
 custom metrics for the neuron project 
 This module is currently very experimental...
-'''
 
+contact: adalca@csail.mit.edu
+'''
+import sys
 import numpy as np
 import keras.backend as K
 
-# medipy package 
-import sys
+# local packages
 import medipy.metrics
 import neuron.src.tftools.metrics
 
+
+
+class CategoricalCrossentropy(object):
+    '''
+    Categorical crossentropy with optional categorical weights and spatial prior
+
+    Adapted from weighted categorical crossentropy via wassname:
+    https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d
+
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+        prior: path to numpy file with variable 'prior' in it,
+            or the ready prior, reshaped for keras model
+
+    Usage:
+        loss = CategoricalCrossentropy().loss # or
+        loss = CategoricalCrossentropy(weights=weights).loss # or
+        loss = CategoricalCrossentropy(..., prior=prior).loss
+        model.compile(loss=loss, optimizer='adam')
+    '''
+
+    def __init__(self, weights=None, prior=None):
+        self.weights = weights if (weights is not None) else K.variable(weights)
+
+        # process prior
+        if prior is not None:
+            if isinstance(prior, str):  # assuming file
+                loc_vol = np.load(prior)['prior']
+                prior = np.expand_dims(loc_vol, axis=0)  # reshape for keras model
+            self.log_prior = K.log(K.clip(K.variable(prior), K.epsilon(), 1))
+        else:
+            self.log_prior = None
+
+    def loss(self, y_true, y_pred):
+        ''' categorical crossentropy loss '''
+        # scale and clip probabilities
+        # this should not be necessary for softmax output.
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        y_pred = K.clip(y_pred, K.epsilon(), 1)
+
+        # compute log probability
+        log_post = K.log(y_pred)  # likelihood
+
+        # add prior to form posterior
+        if self.log_prior is not None:
+            log_post += self.log_prior
+
+        # loss
+        loss = - y_true * log_post
+
+        # weighted loss
+        if self.weights is not None:
+            loss *= self.weights
+
+        # take the mean loss
+        return K.mean(K.sum(loss, -1))
+
+
+
 class Dice(object):
     ''' UNTESTED
+    Currently there is a problem with tf being able to compute derivates for this implementation
+
     Dice-based metric(s)
 
     Variables:
@@ -21,7 +83,6 @@ class Dice(object):
         weights: optional numpy array of shape (L,) giving relative weights of each label
         prior: filename of spatial priors to be added to y_pred before Dice
             TODO: maybe move this to a 'Prior' layer with a set weight in architecture ?
-
 
     Usage
         diceloss = metrics.dice([1, 2, 3])
@@ -66,59 +127,6 @@ class Dice(object):
 
         # return negative mean dice as loss
         return K.mean(-dicem)
-
-
-
-class CategoricalCrossentropy(object):
-    ''' UNTESTED
-    Categorical crossentropy with optional weights and spatial prior
-
-    Adapted from weighted categorical crossentropy via wassname:
-    https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d
-
-    Variables:
-        weights: numpy array of shape (C,) where C is the number of classes
-        prior:
-
-    Usage:
-        loss = CategoricalCrossentropy().loss # or
-        loss = CategoricalCrossentropy(weights=weights).loss # or
-        loss = CategoricalCrossentropy(..., prior=prior).loss
-        model.compile(loss=loss,optimizer='adam')
-    '''
-
-    def __init__(self, weights=None, prior=None):
-        self.weights = weights if (weights is not None) else K.variable(weights)
-
-        # process prior
-        if prior is not None:
-            data = np.load(prior)
-            loc_vol = data['prior']
-            loc_vol = np.expand_dims(loc_vol, axis=0) # reshape for model
-            self.log_prior = K.log(K.clip(K.variable(loc_vol), K.epsilon(), 1))
-        else:
-            self.log_prior = None
-
-    def loss(self, y_true, y_pred):
-        ''' categorical crossentropy loss '''
-        # scale preds so that the class probas of each sample sum to 1
-        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
-        # clip
-        y_pred = K.clip(y_pred, K.epsilon(), 1)
-
-        # combine
-        log_post = K.log(y_pred)
-        if self.log_prior is not None:
-            log_post += self.log_prior
-
-        # calc
-        p = y_true * log_post
-        loss = p
-        if self.weights is not None:
-            loss *= self.weights
-
-        loss = -K.sum(loss, -1)
-        return K.mean(loss)
 
 
 
