ecdfe438e6c9d7389da7cafadea2d59f8864d284
adalca <avd12@tebo.nmr.mgh.harvard.edu>
2020-03-10 15:17:56 -0400

    adding small changes

--- neuron/layers.py
+++ neuron/layers.py
@@ -358,7 +358,8 @@ class VecInt(Layer):
 
         # necessary for multi_gpu models...
         loc_shift = K.reshape(loc_shift, [-1, *self.inshape[1:]])
-        loc_shift._keras_shape = inputs[0]._keras_shape
+        if hasattr(inputs[0], '_keras_shape'):
+            loc_shift._keras_shape = inputs[0]._keras_shape
         
         # prepare location shift
         if self.indexing == 'xy':  # shift the first two dimensions
@@ -371,7 +372,8 @@ class VecInt(Layer):
 
         # map transform across batch
         out = tf.map_fn(self._single_int, [loc_shift] + inputs[1:], dtype=tf.float32)
-        out._keras_shape = inputs[0]._keras_shape
+        if hasattr(inputs[0], '_keras_shape'):
+            out._keras_shape = inputs[0]._keras_shape
         return out
 
     def _single_int(self, inputs):
@@ -852,7 +854,7 @@ class LocallyConnected3D(Layer):
         return output
 
 
-class LocalCrossLinear(keras.layers.Layer):
+class LocalCrossLinear(tensorflow.keras.layers.Layer):
     """ 
     Local cross mult layer
 
