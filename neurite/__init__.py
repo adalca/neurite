08bc48d8c616fdaa968f3f3bfdcb87e5937cd712
adalca <adalca@mit.edu>
2020-08-06 09:39:42 -0400

    rename files

---
+++ neurite/__init__.py
@@ -0,0 +1,15 @@
+# ---- neuron ----
+from . import py
+from .py import utils
+
+
+# import backend-dependent submodules
+backend = py.utils.get_backend()
+if backend == 'pytorch':
+    # the pytorch backend can be enabled by setting the VXM_BACKEND
+    # environment var to "pytorch"
+    from . import torch
+else:
+    # tensorflow is default backend
+    from . import tf
+    from .tf import *
