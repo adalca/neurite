47c43527c6a0e7a700c17d8fe2b1118acfcd726c
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2020-10-09 14:01:58 -0400

    Convert labels to indices.

--- neurite/py/utils.py
+++ neurite/py/utils.py
@@ -25,3 +25,20 @@ def softmax(x, axis):
     """
 
     return np.exp(x) / np.sum(np.exp(x), axis=axis, keepdims=True)
+
+
+def rebase_lab(labels):
+    """
+    Rebase labels and return lookup table (LUT) to convert to new labels in
+    interval [0, N[ as: LUT[label_map]. Be sure to pass all possible labels.
+    """
+    labels = np.unique(labels) # Sorted.
+    assert np.issubdtype(labels.dtype, np.integer), 'non-integer data'
+
+    lab_to_ind = np.zeros(np.max(labels) + 1, dtype='int_')
+    for i, lab in enumerate(labels):
+        lab_to_ind[ lab ] = i
+    ind_to_lab = labels
+
+    return lab_to_ind, ind_to_lab
+
