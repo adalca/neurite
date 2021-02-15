16e8716d24e9d3ed3bd6a5deadb278b319c5085f
Andrew Hoopes <athoopes1@gmail.com>
2021-02-14 19:52:28 -0500

    nf: add utilities for colorizing segmentations

--- neurite/py/utils.py
+++ neurite/py/utils.py
@@ -41,3 +41,53 @@ def rebase_lab(labels):
     ind_to_lab = labels
 
     return lab_to_ind, ind_to_lab
+
+
+def load_label_table(filename):
+    """
+    Reads a label lookup-table from file. File is expected to
+    define the anatomical name and color for each label ID.
+    Each line in the file should have the format:
+
+    ```
+    ID    AnatomicalName    R G B
+    ```
+
+    Parameters:
+        filename (str): File to load.
+    Returns:
+        dict: Label lookup dictionary.
+    """
+    label_table = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.rstrip()
+            if not line or line[0] == '#':
+                continue
+            tokens = line.split()
+            sid = int(tokens[0])
+            name = tokens[1]
+            label_table[sid] = {'name': name}
+            if len(tokens) > 2:
+                label_table[sid]['color'] = [int(c) for c in tokens[2:5]]
+    return label_table
+
+
+def color_seg(seg, label_table):
+    """
+    Converts a hard segmentation into an RGB color image given a
+    label lookup-table dictionary.
+
+    Parameters:
+        seg (ndarray): Hard segmentation array.
+        label_table (dict): Label lookup.
+    Returns:
+        ndarray: RGB (3-frame) image with shape of input seg.
+    """
+    unique = np.unique(seg)
+    color_seg = np.zeros((*seg.shape, 3), dtype='uint8')
+    for sid in unique:
+        label = label_table.get(sid)
+        if label is not None:
+            color_seg[seg == sid] = label['color']
+    return color_seg
