6d312256f8589b76cdcd3a91c388c5accaff5e67
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2022-02-11 09:25:37 -0500

    Fix label indexing for multi-channel case.

--- neurite/tf/models.py
+++ neurite/tf/models.py
@@ -773,18 +773,14 @@ def labels_to_image(
         std_min = [0] + [5] * (num_in_labels - 1)
     if std_max is None:
         std_max = [25] * num_in_labels
-    int_range = [mean_min, mean_max, std_min, std_max]
-    for i, x in enumerate(int_range):
-        x = np.asarray(x)
-        int_range[i] = x[..., None] if np.ndim(x) == 1 else x
-    m0, m1, s0, s1 = int_range
+    m0, m1, s0, s1 = map(np.asarray, (mean_min, mean_max, std_min, std_max))
     mean = tf.random.uniform(
-        shape=(batch_size, num_in_labels, num_chan),
+        shape=(batch_size, num_chan, num_in_labels),
         minval=m0, maxval=m1,
         seed=seeds.get('mean'),
     )
     std = tf.random.uniform(
-        shape=(batch_size, num_in_labels, num_chan),
+        shape=(batch_size, num_chan, num_in_labels),
         minval=s0, maxval=s1,
         seed=seeds.get('std'),
     )
