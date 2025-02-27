b67ab5d88e3330c8d69d77701805d6445a1b1b75
Etienne Chollet <etiennepchollet@gmail.com>
2025-02-27 12:48:55 -0500

    [REFACTOR] Remove wildcard imports from util.py's __init__.py

--- neurite/pytorch/utils/__init__.py
+++ neurite/pytorch/utils/__init__.py
@@ -4,6 +4,3 @@ This package contains various torch utilities.
 
 from . import augment           # noqa: F401
 from . import utils             # noqa: F401
-
-from .augment import *          # noqa: F401, F403
-from .utils import *            # noqa: F401, F403
