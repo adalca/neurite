8166eecd08df24bde92247a16c40fbe150b88b0b
Etienne Chollet <etiennepchollet@gmail.com>
2025-02-27 19:25:29 -0500

    [REFACTOR] Do not unsqueeze inputs to cross_expand()

--- neurite/pytorch/utils/utils.py
+++ neurite/pytorch/utils/utils.py
@@ -1426,13 +1426,13 @@ def cross_expand(
     torch.Size([1, 3, 7, 4, 5, 6]) torch.Size([1, 3, 7, 8, 9, 10])
     """
 
-    # Make some room for the slices! (Sx & Sy)
-    x1 = x1.unsqueeze(0)
-    x2 = x2.unsqueeze(0)
-
     # Unpack to get Sx1 and Sx2 slice dimensions
     Bx1, Sx1, Cx1, *x1_spatial = x1.shape  # Could've used x1.size(1), but I like it this way :)
     Bx2, Sx2, Cx2, *x2_spatial = x2.shape
+
+    if Bx1 != Bx2:
+        raise ValueError(
+            f"The input tensors must have the same number of batches. Got Bx1={Bx1} and Bx2={Bx2}")
 
     # n-Dimensional reshaping/cartesian product of tensors
     x1_expanded = einops.repeat(x1, "Bx1 Sx1 Cx1 ... -> Bx1 Sx1 Sx2 Cx1 ...", Sx2=Sx2)
