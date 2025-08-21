e0fb7db7888afc2246b9e18b4df730fdf4811328
Etienne Chollet <etiennepchollet@gmail.com>
2025-08-21 15:16:08 -0400

    [tests] for explicit, nonexplicit, and primary module imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -12,5 +12,45 @@ def test_nonexplicit_imports():
     assert hasattr(ne.plot, 'volume3D')
     _ = ne.plot.volume3D
 
-    # Try initializing a `gaussian_kernel`
+    # utils.py
     _ = ne.utils.gaussian_kernel(42)
+    _ = ne.utils.barycenter
+    _ = ne.utils.interpn
+    _ = ne.utils.volshape_to_meshgrid
+    _ = ne.utils.resize
+
+
+def test_explicit_submodule_imports():
+    import neurite as ne
+    _ = ne.py.utils.normalize_axes
+
+
+
+def test_primary_modules():
+
+    import neurite as ne
+
+    # metrics.py
+    _ = ne.callbacks.ModelCheckpointParallel
+
+    # layers.py
+    _ = ne.layers.SampleNormalLogVar
+    _ = ne.layers.Negate
+    _ = ne.layers.Constant
+    _ = ne.layers.LocalParamWithInput
+    _ = ne.layers.MeanStream
+    _ = ne.layers.HyperConvFromDense
+
+    # modelio.py
+    _ = ne.modelio.LoadableModel
+    _ = ne.modelio.LoadableModel.ReferenceContainer
+    _ = ne.modelio.store_config_args
+
+    # models.py
+    _ = ne.models.labels_to_image
+    _ = ne.models.labels_to_image_old
+    _ = ne.models.conv_dec
+
+    # metrics.py
+    _ = ne.metrics.MutualInformation
+
