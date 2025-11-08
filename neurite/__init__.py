d097aa9b0499da06e3d5a6564dd8253b28bcecce
Etienne Chollet <etiennepchollet@gmail.com>
2025-11-07 21:59:23 -0500

    [refactor] add functional import to base package

--- neurite/__init__.py
+++ neurite/__init__.py
@@ -73,6 +73,7 @@ from . import py        # Noqa: F401
 from . import nn
 from . import utils
 from . import samplers
+from . import functional
 
 # Importing submodules from `py` to top level. Not `utils` to avoid shadowing
 from .py import data, dataproc, plot
